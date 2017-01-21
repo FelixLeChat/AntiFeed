80c9ea1a1cbaca5a06027d03a5fcb60a81fe46ce
FelixLeChat <felixlrc21@gmail.com>
2017-01-21 12:48:44 -0500

    Add materialize UI

---
+++ AntiFeed/src/Components/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+      	<AppBar title="AntiFeed - We burst your social bubble" />
+      </div>
+    );
+  }
+}
+
+export default Header;
